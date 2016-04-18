b405ff56115125140c65e8b9280a88136aab1b36
Robbie McKinstry <rem89@pitt.edu>
2016-04-17 21:34:16 -0400

    i think this actually works

--- droplet.tf
+++ droplet.tf
@@ -1,23 +1,17 @@
 resource "digitalocean_droplet" "docker-agent" {
-    image = "coreos-899.15.0"
+    image = "coreos-stable"
     name = "docker-agent"
     region = "nyc2"
     size = "512mb"
     private_networking = true
-    ssh_keys = [
-      "${var.ssh_fingerprint}"
-    ]
+
+    ssh_keys = [1827636]
 
     connection {
         user = "root"
         type = "ssh"
-        key_file = "${var.pvt_key}"
+        agent = false
+        private_key = "${file("/Users/robbiemckinstry/.ssh/digital_ocean")}"
         timeout = "2m"
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "docker ip"
-        ]
     }
 }
