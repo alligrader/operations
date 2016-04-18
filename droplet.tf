4417ae52a74ed91eb3f437b3862b230c385e5018
Robbie McKinstry <rem89@pitt.edu>
2016-04-18 10:17:07 -0400

    replicating the swarm followers

--- droplet.tf
+++ droplet.tf
@@ -1,6 +1,7 @@
-resource "digitalocean_droplet" "docker-agent" {
-    image = "coreos-stable"
-    name = "docker-agent"
+resource "digitalocean_droplet" "swarm-leader" {
+    # image = "coreos-stable"
+    image = "ubuntu-14-04-x64"
+    name = "swarm-leader"
     region = "nyc2"
     size = "512mb"
     private_networking = true
@@ -13,5 +14,66 @@ resource "digitalocean_droplet" "docker-agent" {
         agent = false
         private_key = "${file("/Users/robbiemckinstry/.ssh/digital_ocean")}"
         timeout = "2m"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo apt-get update -q",
+            "sudo apt-get install -yq curl",
+            "curl -sSL https://get.docker.com/ | sh",
+            "docker run --rm swarm create"
+        ]
+    }
+}
+
+resource "digitalocean_droplet" "swarm-follower1" {
+    image = "ubuntu-14-04-x64"
+    name = "swarm-follower1"
+    region = "nyc2"
+    size = "512mb"
+    private_networking = true
+
+    ssh_keys = [1827636]
+
+    connection {
+        user = "root"
+        type = "ssh"
+        agent = false
+        private_key = "${file("/Users/robbiemckinstry/.ssh/digital_ocean")}"
+        timeout = "2m"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo apt-get update -q",
+            "sudo apt-get install -yq curl",
+            "curl -sSL https://get.docker.com/ | sh",
+        ]
+    }
+}
+
+resource "digitalocean_droplet" "swarm-follower2" {
+    image = "ubuntu-14-04-x64"
+    name = "swarm-follower2"
+    region = "nyc2"
+    size = "512mb"
+    private_networking = true
+
+    ssh_keys = [1827636]
+
+    connection {
+        user = "root"
+        type = "ssh"
+        agent = false
+        private_key = "${file("/Users/robbiemckinstry/.ssh/digital_ocean")}"
+        timeout = "2m"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo apt-get update -q",
+            "sudo apt-get install -yq curl",
+            "curl -sSL https://get.docker.com/ | sh",
+        ]
     }
 }
